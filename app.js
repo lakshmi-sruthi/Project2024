f7db788ce4892f1cb0819ddd78aafe006ceb6913
lakshmi-sruthi <lakshmi-sruthi@users.noreply.github.com>
2024-03-04 18:29:53 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//imports express.js web framework into node.js application
+//variable express contains functionality to create servers, define routes , handle http req etc
+
+const express = require('express');
+const app = express(); //newly created express application instance
+const chalk = require('chalk'); //debugging 
+const path = require('path');
+const morgan = require('morgan');
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('tiny')); //HTTP request logger middleware for Node. js
+
+//sets up middleware to serve static files (eg html)
+app.use(express.static(path.join(__dirname,'/public/')));
+
+app.get('/',(req , res) => {
+    res.send('hello sruthi')
+})
+
+app.listen(PORT,()=>{
+    console.log(`listening to port ${chalk.green(PORT)}`);
+});
+
+//to run app ,command node app.js or npm start
+//everytime change is made nodemon monitors and restarts the app
