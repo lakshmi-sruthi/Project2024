cf1a445c4cfc707268f924e04630a03403969ed5
lakshmi-sruthi <lakshmi-sruthi@users.noreply.github.com>
2024-03-07 22:34:09 +0530

    adding reactivity and some  image icons

--- client/src/Components/LoginSignup/LoginSignup.jsx
+++ client/src/Components/LoginSignup/LoginSignup.jsx
@@ -1,32 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
 
 const LoginSignup = () => {
+
+    const [action,setAction] = useState("Login");
+
   return (
     <div class="container">
       <div className="header">
         <div className="text">
-            Sign up
+            {action}
         </div>
+        <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input">
-            <i class="fas fa-user"></i>
+            {action==="Login"?<div></div>: 
+            <div className="input">
+                <img src={user_icon} alt="" />
             <input type="text" placeholder = "Name"/>
-        </div>
+        </div>}
         <div className="input">
-            <i class="fas fa-envelope"></i>
+            <img src={email_icon} alt="" />
             <input type="email" placeholder = "Email ID" />
         </div>
         <div className="input">
-            <i class="fas fa-lock"></i>
+            <img src={password_icon} alt="" />
             <input type="password" placeholder = "Password"/>
         </div>
       </div>
-      <div className="forgot-password"><span>Lost password? Click Here!</span></div>
+      {action==="Login"?<div className="forgot-password"><span>Lost password? Click Here!</span></div> : <div></div>}
       <div className="submit-container">
-        <div className="submit">Signup</div>
-        <div className="login">Login</div>
+        <div className={action==="Login" ? "submit gray":"submit"} onClick={()=>{setAction("Sign up")}}>Signup</div>
+        <div className={action==="Sign up" ? "submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
       </div>
     </div>
   )
